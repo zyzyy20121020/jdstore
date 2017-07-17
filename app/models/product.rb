99ff9b671831f8ece2e8c410ac0214469a1ef60a
ReadingPig <zyzpop830203@163.com>
2017-07-17 14:47:19 +0800

    image uploader complete

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   validates :quantity, presence: true
   validates :price, presence: true
   validates :price, numericality: { greater_than: 0}
+
+  mount_uploader :image, ImageUploader
 end
