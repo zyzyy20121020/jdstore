d779233f9729d1fd232349abd84f1f2297dede16
ReadingPig <zyzpop830203@163.com>
2017-07-17 22:26:16 +0800

    add layout at backend

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -2,6 +2,7 @@ class Admin::ProductsController < ApplicationController
   before_action :authenticate_user!
   before_action :admin_required
 
+  layout "admin"
 
   def new
     @product = Product.new
