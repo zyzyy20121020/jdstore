6ca83eb3e49031ab13f93e148f2996c785926ccf
ReadingPig <zyzpop830203@163.com>
2017-07-17 22:22:30 +0800

    add another admin at seed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def admin_required
     if !current_user.admin?
-      flash[:alert] = 'You are not admin'
+      redirect_to "/", flash[:alert] = 'You are not admin'
       redirect_to root_path
     end
   end
