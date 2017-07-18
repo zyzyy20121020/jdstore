ef523de999e8d73aad26685b6a8ad960e70021b0
ReadingPig <zyzpop830203@163.com>
2017-07-18 21:11:12 +0800

    implement add_product_to_cart_function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170717062403) do
+ActiveRecord::Schema.define(version: 20170718130359) do
+
+  create_table "cart_items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
